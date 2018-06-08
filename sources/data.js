842f7c75840c41ed164843b28894d73a435c73a1
javaking2013 <patrick.r.meadows@gmail.com>
2018-06-07 21:31:54 -0500

    Committing incremental.

--- sources/data.js
+++ sources/data.js
@@ -10,3 +10,14 @@ let carLoan = {
     monthly : 261.78
 };
 
+
+
+/*
+3- best buy credit card
+4- discover card
+5- van
+6- patrick student loan
+7- heidi student loan
+8- mortgage
+9- llr loan
+ */
